d98d6dc96b2177813283cbba93cfc099792dbdae
aacohen <arielaades@gmail.com>
2016-09-29 22:29:25 -0400

    Test errors on qualified tutor and skills methods

--- swift-people/Person.swift
+++ swift-people/Person.swift
@@ -11,20 +11,59 @@ import Foundation
 class Person {
     let name: String
     var ageInYears: Int?
-    var skills: [String] = []
-    var qualifiedTutor: Bool { return false }
-    
+    var skills: Set<String> = []
+    var qualifiedTutor: Bool { return skills.count >= 4 }
     init(name: String, ageInYears: Int?) {
         self.name = name
         self.ageInYears = ageInYears
     }
     
-    convenience init(name: String, ageinYears: Int?) {
+    convenience init(name: String) {
         self.init(name: name, ageInYears: nil)
         self.ageInYears = nil
     }
     init() {
         self.name = "John Doe"
+    }
+     //III.
+    
+    func celebrateBirthday () -> String {
+        if ageInYears == nil {
+          let birthdayName = self.name.uppercased()
+            return "HAPPY BIRTHDAY, \(birthdayName)"
+          }
+        else {
+//            //if let ageInYears = self.ageInYears  {
+//              var age = ageInYears += 1
+//            }
+//            var ext = age.ordinal.uppercased()
+//            let birthdayName = self.name.uppercased()
+            return ("HAPPY BIRTHDAY, )!!")
+            //how to put .ordinal in here. it doesn't auto complete?
+        }
+       return ""
+    }
+    
+    //IV. skills methods
+    
+    func learnSkillsBash() {
+    skills.insert("bash")
+    }
+    
+    func learnSkillXcode() {
+        skills.insert("Xcode")
+    }
+    
+    func learnSkillObjectiveC () {
+        skills.insert("Objective-C")
+    }
+    
+    func learnSkillSwift() {
+        skills.insert("Swift")
+    }
+    
+    func learnSkillInterfaceBuilder() {
+        skills.insert("Interface Builder")
     }
     
     
